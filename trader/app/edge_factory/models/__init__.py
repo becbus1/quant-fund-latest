e41141a9eae4f5960b295e6ba01ea686a1a9db6a
becbus1 <beckettbusiness1@gmail.com>
2025-12-23 15:20:15 -0500

    Update __init__.py

--- trader/app/edge_factory/models/__init__.py
+++ trader/app/edge_factory/models/__init__.py
@@ -1,5 +1,5 @@
-# edge_factory.models package
-from edge_factory.models.anomaly_iforest import AnomalyDetector
-from edge_factory.models.meta_labeler import MetaLabeler
+# trader.app.edge_factory.models package
+from trader.app.edge_factory.models.anomaly_iforest import AnomalyDetector
+from trader.app.edge_factory.models.meta_labeler import MetaLabeler
 
 __all__ = ["AnomalyDetector", "MetaLabeler"]
